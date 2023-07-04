93ccdcf51cab05ccb7ce951b34d43b229ecf09e6
kanies89 <krzysztof.kaniewski@gmail.com>
2023-07-04 16:32:42 +0200

    Cleaning

---
+++ Query/test_query.sql
@@ -0,0 +1,22 @@
+INSERT INTO #test_table (id, name, age)
+VALUES
+    (1, 'John', 25),
+    (2, 'Jane', 30),
+    (3, 'Mike', 40),
+    (4, 'Emily', 28),
+    (5, 'Michael', 32),
+    (6, 'Sophia', 45),
+    (7, 'William', 50),
+    (8, 'Olivia', 27),
+    (9, 'Daniel', 35),
+    (10, 'Emma', 31),
+    (11, 'James', 42),
+    (12, 'Grace', 29),
+    (13, 'Benjamin', 37),
+    (14, 'Ava', 26),
+    (15, 'Jacob', 33),
+    (16, 'Mia', 38),
+    (17, 'Ethan', 24),
+    (18, 'Lily', 41),
+    (19, 'Alexander', 36),
+    (20, 'Sofia', 39);
---
+++ test_main.py
@@ -0,0 +1,32 @@
+import pytest
+import pandas as pd
+from connect import connect, connect_single_query
+
+
+def test_connect():
+    # Prepare test data
+    temp_table_file = './query/test_temp.sql'
+    query_file = './query/test_query.sql'
+
+    # Call the function under test
+    result = connect(temp_table_file, query_file)
+
+    # Perform assertions on the result
+    assert isinstance(result, list)
+    assert len(result) > 0
+    for df in result:
+        assert isinstance(df, pd.DataFrame)
+
+
+def test_connect_single_query():
+    # Prepare test data
+    query = 'SELECT * FROM #test_table'
+
+    # Call the function under test
+    result = connect_single_query(query)
+
+    # Perform assertions on the result
+    assert isinstance(result, list)
+    assert len(result) > 0
+    for df in result:
+        assert isinstance(df, pd.DataFrame)
