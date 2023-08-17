8af3f9e7e720983c7c5f476557213b6d1e781ecf
kanies89 <krzysztof.kaniewski@gmail.com>
2023-08-17 16:12:34 +0200

    Adding tests for AR2

--- ui.py
+++ ui.py
@@ -2,8 +2,12 @@ import sys
 import datetime
 from PyQt5.QtWidgets import QApplication, QDialog, QLineEdit
 from PyQt5.QtCore import pyqtSignal, Qt, QEvent, QThread, pyqtSlot
+from PyQt5.QtGui import QColor
 from PyQt5.uic import loadUi
-from main import start_automation, Logger
+from main import start_automation, Logger, check_quarter
+from check import rule_1
+from variables import EXCEL_READ_AR2
+import pandas as pd
 
 
 class AutomationThread(QThread):
@@ -108,21 +112,55 @@ class MyDialog(QDialog):
         with open(log_file_name, 'w') as log_file:
             log_file.write(self.TDisplay.toPlainText())
 
+    # Method to change the background color of a cell
+    def change_cell_background(self, row, col, r, g, b):
+        item = self.tableWidget.item(row, col)
+        if item:
+            item.setBackground(QColor(r, g, b))
+
     def setup_table(self):
         # Set default alignment for wrapped text in header
         for col in range(self.tableWidget.columnCount()):
             header_item = self.tableWidget.horizontalHeaderItem(col)
             if header_item:
                 header_item.setTextAlignment(Qt.AlignHCenter | Qt.TextWordWrap)
+
+        # Enable word wrapping for all cells
+        for row in range(self.tableWidget.rowCount()):
+            for col in range(self.tableWidget.columnCount()):
+                item = self.tableWidget.item(row, col)
+                if item:
+                    item.setTextAlignment(Qt.AlignVCenter | Qt.TextWordWrap)
+                    self.tableWidget.setItem(row, col, item)
+                    if col > 1:  # Adjust the column index where alignment should be set
+                        item.setTextAlignment(
+                            Qt.AlignVCenter | Qt.AlignHCenter)  # Align center for columns 2 and onward
+
+        # Connect cell content change signal to row height adjustment
+        self.tableWidget.cellChanged.connect(self.adjust_row_heights)
+
         # Set the width and height of the QTableWidget
-        self.tableWidget.setFixedSize(1109, 470)  # Adjust the values as needed
+        self.tableWidget.setFixedSize(1385, 470)  # Adjust the values as needed
+        self.tableWidget.setColumnWidth(0, 100)
+        self.tableWidget.setColumnWidth(1, 210)
+        self.tableWidget.setColumnWidth(3, 50)
+        self.tableWidget.setColumnWidth(4, 50)
+        self.tableWidget.setColumnWidth(6, 95)
+        self.tableWidget.setColumnWidth(10, 100)
+        self.tableWidget.setColumnWidth(11, 115)
+        self.tableWidget.setColumnWidth(12, 60)
+        self.tableWidget.setColumnWidth(14, 50)
+
+    def adjust_row_heights(self, row, col):
+        # Adjust the row height based on the contents of the specified cell
+        self.tableWidget.resizeRowToContents(row)
 
     def toggle_window_size(self):
         if self.enlarged:
             self.setFixedSize(402, 528)  # Set your original size
             self.enlarged = False
         else:
-            self.setFixedSize(1520, 528)  # Set the enlarged size
+            self.setFixedSize(1800, 528)  # Set the enlarged size
             self.enlarged = True
 
     def eventFilter(self, obj, event):
@@ -157,6 +195,8 @@ class MyDialog(QDialog):
         self.BName.setEnabled(False)
         self.TSurname.setEnabled(True)
         self.BSurname.setEnabled(True)
+
+        self.perform_tests()
 
     def on_surname_apply_clicked(self):
         surname = self.TSurname.toPlainText()
@@ -223,11 +263,37 @@ class MyDialog(QDialog):
         # Update the progress bar
         self.progressBar.setValue(progress)
 
+    def append_text_to_cell(self, row, col, text_to_append):
+        current_item = self.table_widget.item(row, col)
+        if current_item:
+            current_text = current_item.text()
+            new_text = current_text + " " + text_to_append
+            current_item.setText(new_text)
+
     def on_automation_finished(self):
         # Enable the "Start" button when the automation is finished
         self.Start.setEnabled(True)
         # Save the logs to the log file
         self.save_logs()
+
+    def perform_tests(self):
+        date = check_quarter()
+        # path = f'EXAMPLE\\Filled\\' + f'BSP_AR2_v.4.0_Q{date[3]}{datetime.date.today().strftime("%Y")}_{datetime.date.today().strftime("%Y%m%d")}.xlsx'
+        path = "C:\\Users\\Krzysztof kaniewski\\PycharmProjects\\pythonProject\\Example\\Filled\\BSP_AR2_v.4.0_Q22023_20230712.xlsx"
+        # Perform all the tests
+        df_nbp = pd.read_excel(path, sheet_name=EXCEL_READ_AR2, header=None, keep_default_na=False)
+
+        bool_1 = True
+        for n in range(5, 13):
+            print("wtf")
+            for result in rule_1(df_nbp, EXCEL_READ_AR2[n - 4], n):
+                print(EXCEL_READ_AR2[n - 4])
+                if not result[1]:
+                    self.change_cell_background(0, n, 255, 0, 0)
+                    # self.append_text_to_cell(n, result[2], f'; Error in column: {result[2]}; ')
+                    bool_1 = False
+                if n == 12 and bool_1:
+                    self.change_cell_background(0, n, 50, 205, 50)
 
 
 if __name__ == "__main__":
